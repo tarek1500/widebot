2068fa8d420149490a50f4f7c66d89227c928e91
Tarek Ibrahem Mohammed <tarek_mohammed2@yahoo.com>
2024-06-03 20:14:50 +0300

    Add auth service
    - Add login function.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideStore } from '@ngrx/store';
+import { provideHttpClient } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
@@ -8,6 +9,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
+        provideHttpClient(),
         provideStore(),
         provideEffects(),
         provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../data/user'
+import { Observable, of, throwError } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    constructor(protected http: HttpClient) { }
+
+    login(email: string, password: string): Observable<User> {
+        let user: User | null = null;
+
+        if (email === 'user@domain.com') {
+            user = {
+                name: 'User',
+                email: 'user@domain.com',
+                phone: '01112345',
+                role: 'user'
+            };
+        }
+        else if (email === 'admin@domain.com') {
+            user = {
+                name: 'Admin',
+                email: 'admin@domain.com',
+                phone: '01167890',
+                role: 'admin'
+            };
+        }
+
+        if (user) {
+            return of(user);
+        }
+
+        return throwError(() => new Error("Credential doesn't match our records"));
+    }
+}
