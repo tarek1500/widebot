c424429e64e4d4f81da9ca7e52f9d5138ea4c8c0
Tarek Ibrahem Mohammed <tarek_mohammed2@yahoo.com>
2024-06-03 20:20:07 +0300

    Use action to login user

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../state';
+import * as appActions from '../../state/app.actions';
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [],
+    imports: [CommonModule, ReactiveFormsModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss'
 })
 export class LoginComponent {
     loginForm = new FormGroup({
         email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required, Validators.minLength(4)])
+    });
 
+    constructor(private store: Store<fromApp.State>) { }
+
+    onSubmit() {
+        if (this.loginForm.valid) {
+            this.store.dispatch(new appActions.Login({
+                email: this.loginForm.value.email!,
+                password: this.loginForm.value.password!
+            }));
+        }
+    }
 }
