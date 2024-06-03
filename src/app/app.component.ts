03c55fd68870d585e9835979fabcd35141822c41
Tarek Ibrahem Mohammed <tarek_mohammed2@yahoo.com>
2024-06-03 20:21:32 +0300

    Handle auth for app component
    - Add conditions to show links.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as fromApp from './state';
+import { User } from './data/user';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, RouterModule, NgbNavModule],
+    imports: [CommonModule, RouterOutlet, RouterModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
-export class AppComponent {
-    title = 'widebot';
+export class AppComponent implements OnInit {
+    user$?: Observable<User | null>;
+
+    constructor(private store: Store<fromApp.State>) { }
+
+    ngOnInit(): void {
+        this.user$ = this.store.pipe(select(fromApp.getCurrentUser));
+    }
 }
