7947f32d96a84d48e4106cc8b961ee36fe57acf2
Tarek Ibrahem Mohammed <tarek_mohammed2@yahoo.com>
2024-06-04 14:11:32 +0300

    Add load users

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as fromApp from '../../state';
+import * as appActions from '../../state/app.actions';
+import { User } from '../../data/user';
 
 @Component({
-  selector: 'app-admin',
-  standalone: true,
-  imports: [],
-  templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+    selector: 'app-admin',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './admin.component.html',
+    styleUrl: './admin.component.scss'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+    users$?: Observable<User[]>;
+    error$?: Observable<string>;
 
+    constructor(private store: Store<fromApp.State>) { }
+
+    ngOnInit(): void {
+        this.users$ = this.store.pipe(select(fromApp.getUsers));
+        this.error$ = this.store.pipe(select(fromApp.getLoadError));
+
+        this.store.dispatch(new appActions.Load);
+    }
 }
