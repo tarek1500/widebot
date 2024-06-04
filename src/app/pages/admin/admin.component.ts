c57611036f15cd58b45511660c8cf52d5320233e
Tarek Ibrahem Mohammed <tarek_mohammed2@yahoo.com>
2024-06-04 14:14:31 +0300

    Add create user
    - Add modal to create user.

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import * as fromApp from '../../state';
 import * as appActions from '../../state/app.actions';
 import { User } from '../../data/user';
@@ -9,20 +11,44 @@ import { User } from '../../data/user';
 @Component({
     selector: 'app-admin',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, ReactiveFormsModule],
     templateUrl: './admin.component.html',
     styleUrl: './admin.component.scss'
 })
 export class AdminComponent implements OnInit {
     users$?: Observable<User[]>;
+    createForm = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        username: new FormControl('', [Validators.required]),
+        phone: new FormControl('', [Validators.required])
+    });
     error$?: Observable<string>;
 
-    constructor(private store: Store<fromApp.State>) { }
+    constructor(private store: Store<fromApp.State>, private modalService: NgbModal) { }
 
     ngOnInit(): void {
         this.users$ = this.store.pipe(select(fromApp.getUsers));
         this.error$ = this.store.pipe(select(fromApp.getLoadError));
 
         this.store.dispatch(new appActions.Load);
+    }
+
+    openCreateModal(content: TemplateRef<any>) {
+        this.modalService.open(content)
+            .result
+            .then(
+                result => {
+                    this.store.dispatch(new appActions.Create({
+                        id: 0,
+                        name: this.createForm.value.name!,
+                        email: this.createForm.value.email!,
+                        username: this.createForm.value.username!,
+                        phone: this.createForm.value.phone!,
+                        role: 'user'
+                    }));
+                },
+                reason => { }
+            );
     }
 }
